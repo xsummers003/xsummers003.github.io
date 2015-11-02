d216ffad571c7d9990f2b3cf07560e0841ab90b5
nguyenav9 <nguyenav9@gmail.com>
2015-11-01 21:45:50 -0500

    Setup database connection

---
+++ DataAccess/DatabaseLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class DatabaseLayer
+    {
+        private string conn = ConfigurationManager.ConnectionStrings["scheduler_database"].ToString();
+
+        public void InsertUpdateDeleteSQLString(string sqlstring)
+        {
+            SqlConnection objsqlconn = new SqlConnection(conn);
+            objsqlconn.Open();
+            SqlCommand objcmd = new SqlCommand(sqlstring, objsqlconn);
+            objcmd.ExecuteNonQuery();
+        }
+    }
+}
