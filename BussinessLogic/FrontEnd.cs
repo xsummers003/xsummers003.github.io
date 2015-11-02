ecd5ee30799b5c15c5ef4f2581b3fe0a917d8be8
stoplime <slanimero@gmail.com>
2015-11-01 19:57:07 -0500

    Contained it in the Project

---
+++ BussinessLogic/FrontEnd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class FrontEnd
+    {
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public FrontEnd()
+        {
+
+        }
+    }
+}
